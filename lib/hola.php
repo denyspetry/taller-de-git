db70455c7f6d9e0d532b1444085cc18601c00543
Denys <denyspetrychenko@gmail.com>
2025-12-17 13:45:23 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,7 @@
 <?php
     // Autor: Denys Petrychenko <denyspetrychenko@gmail.com>
     // El nombre por defecto es Mundo
-    $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+    print "Introduce tu nombre:";
+    $nombre = trim(fgets(STDIN));
     @print "Hola, {$nombre}\n";
 ?>
